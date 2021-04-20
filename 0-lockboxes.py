01d75ea68b8ed5239ed20d19d4dbfd52978ced73
jesgogu27 <jesgogu@hotmail.com>
2021-04-20 00:21:57 -0500

    interview

---
+++ 0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1
+    and each box may contain keys to the other boxes.
+    """
+    keys = [0]
+    for b in keys:
+        for key in boxes[b]:
+            if key not in keys:
+                if key < len(boxes):
+                    keys.append(key)
+
+    if len(keys) == len(boxes):
+        return True
+    return False
